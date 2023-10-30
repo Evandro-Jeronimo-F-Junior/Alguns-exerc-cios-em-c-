18a85e7a085cdb611f63f01d0f284bb488c47d15
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2023-10-30 12:05:20 -0300

    Uso simples do polimorfismo

---
+++ poliformismo.cs
@@ -0,0 +1,32 @@
+﻿class Paibarulhopet
+{
+    public virtual void Som()
+    {
+        Console.WriteLine("au");
+    }
+}
+class gato:Paibarulhopet
+{
+    static void Main()
+    {
+        gato Gato = new gato();
+        Paibarulhopet Cachorro = new cachorro();
+        Gato.Som();
+        Cachorro.Som();
+    }
+    public override void Som()
+    {
+      
+        Console.WriteLine("miau");
+        
+    }
+}
+
+
+class cachorro:Paibarulhopet
+{
+    public override void Som()
+    {
+        Console.WriteLine("ROOF, ROOF");
+    }
+}
