2437b17d14b82294c547fe9aa93ac92d6d341ad0
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2023-11-02 21:08:15 -0300

    Um leve exercicio sobre Abstraççcao

---
+++ Abstraçao_e_herança.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        cervejas heineken = new cerveja1();
+        cervejas stella = new cerveja2();
+        heineken.Tipo();
+        stella.Tipo();
+    }
+}
+public class cerveja1:cervejas
+{
+    public override void Tipo()
+    {
+        Console.WriteLine("heineken");
+    }
+}
+public class cerveja2:cervejas
+{
+    public override void Tipo()
+    {
+        Console.WriteLine("Stella");
+    }
+}
+
+public abstract class cervejas
+{
+    public abstract void Tipo();
+}
