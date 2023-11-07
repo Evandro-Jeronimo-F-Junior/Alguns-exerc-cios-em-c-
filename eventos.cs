d229e3934c14735543b78b94ff858f4f2370d504
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2023-11-07 11:27:12 -0300

    Uso Simples de evento

---
+++ eventos.cs
@@ -0,0 +1,43 @@
+﻿class Contador
+{
+    private int contador {get; set;}
+    
+    public event Action EventoContador = delegate{};
+
+    public void DispararEvento()
+    {
+        EventoContador?.Invoke();
+    }
+    public Contador(int ValorInicial)
+    {
+        contador = ValorInicial;
+
+    }
+}
+
+class Incrito
+{
+    public string Nome {get; set;}
+
+    public Incrito(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void Mensagem()
+    {
+        Console.WriteLine($"evento disparado para o {Nome}");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Incrito inscrito1 = new Incrito("Marcus");
+        Contador cont = new Contador(1);
+        cont.EventoContador += inscrito1.Mensagem;
+        cont.DispararEvento();
+    }
+}
