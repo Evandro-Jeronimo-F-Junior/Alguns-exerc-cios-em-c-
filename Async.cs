7b359f99d1f7bda2ec0858c4f08567887f72a1d8
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2023-12-06 17:54:51 -0300

    Um exercicio assincrono simples

---
+++ Async.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Task Contagem1 = Contagem("Contagem 1", 10);
+        Task Contagem2 = Contagem("Contagem 2", 15);
+        await Task.WhenAll(Contagem1, Contagem2);
+
+        Console.WriteLine("As contagens Acabaram");
+    }
+
+    static async Task Contagem(string texto, int contador)
+    {
+        for(int i = 1; i <= contador; i++)
+        {
+            Console.WriteLine($"{texto} é {i}");
+
+            await Task.Delay(100);
+        }
+    }
+}
